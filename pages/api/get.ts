c8bb73ae367feb6a63445ec13328f338b52a8364
Rodrigo-Portto <porttorodrigo@gmail.com>
2026-04-26 20:10:48 -0300

    fix: reformat get.ts createClient syntax to fix TypeScript error

--- pages/api/get.ts
+++ pages/api/get.ts
@@ -1,25 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  				process.env.SUPABASE_SECRET_KEY!
-  { auth: { autoRefreshToken: false, persistSession: false } }
-)
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET_KEY!, {
+  auth: { autoRefreshToken: false, persistSession: false }
+})
 
 const getBrandingModels = async () => {
-  const { data, error } = await supabase
-    .from('plataforma_marca')
-    .select('*')
-
+  const { data, error } = await supabase.from('plataforma_marca').select('*')
   if (error) throw new Error(JSON.stringify(error))
   return data
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const data = await getBrandingModels()
     res.status(200).json(data)
